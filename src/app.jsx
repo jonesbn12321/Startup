f0f65b75bb4e9ca8f0751028ee64af9c36016e32
Brooklyn Jones <jonesbn12321@gmail.com>
2026-02-05 11:38:24 -0700

    run with dev

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <div className="body">App will display here</div>;
 }
