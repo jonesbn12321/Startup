a83f05adb10c7e53b51bf3267d2f4d4283544518
Brooklyn Jones <jonesbn12321@gmail.com>
2026-02-06 12:01:20 -0700

    add jsx component code

---
+++ src/opponents/opponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Opponents() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>opponents displayed here</div>
+    </main>
+  );
+}
