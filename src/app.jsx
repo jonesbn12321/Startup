8890e53fe72a392cab2bdb9a0e07d5975cfafc7f
Brooklyn Jones <jonesbn12321@gmail.com>
2026-02-06 22:13:17 -0700

    Update navbar

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,47 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { Opponents } from './opponents/opponents';
 
 export default function App() {
 
   return (
-    <div className="app bg-dark text-light">
-      <header>
-          <h1>Monster Matching</h1>
-          {/* <!-- Navigation Elements to get to each page --> */}
-            <nav>
-              <menu>
-                  <li><a href = "index.html"className = "active">Home</a></li>
-                  <li><a href = "opponents.html">Choose Opponent</a></li>
-                  <li><a href = "scores.html">Scores</a></li>
-              </menu>
-            </nav>
-      </header>
+    <BrowserRouter>
     
-      <main>App Components here</main>
+      <div className="app bg-dark text-light">
+        <header>
+            <h1>Monster Matching</h1>
+            {/* <!-- Navigation Elements to get to each page --> */}
+              <nav>
+                <menu>
+                    <li><NavLink className={({ isActive }) => isActive ? "nav-button active" : "nav-button"} to="/">Login</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "nav-button active" : "nav-button"} to="/opponents">Opponents</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? "nav-button active" : "nav-button"} to="/scores">Scores</NavLink></li>                </menu>
+              </nav>
+        </header>
+      
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/opponents' element={<Opponents />} />
+          <Route path='/scores' element={<Scores />} />
+          <Route path='/play' element={<Play />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-      <footer>
-        <hr />
-        <p >Brooklyn Jones</p>
-        <a href = "https://github.com/jonesbn12321/Startup">GitHub</a>
-      </footer>
-    </div>
+        <footer>
+          <hr />
+          <p >Brooklyn Jones</p>
+          <a href = "https://github.com/jonesbn12321/Startup">GitHub</a>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
