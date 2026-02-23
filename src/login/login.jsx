dd0ea38319cd1046db2975f7d9180fe612bf6aaf
Brooklyn Jones <jonesbn12321@gmail.com>
2026-02-23 16:46:10 -0700

    Add login to local storage

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 export function Login() {
+    const [name, setName] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    function loginUser(){
+        console.log('login '+name + " "+password);
+        localStorage.setItem('user', name, password);
+    }
+
+    function nameChange(e){
+        setName(e.target.value);
+    }
+    function passChange(e){
+        setPassword(e.target.value);
+    }
   return (
     <main className = "container py-4">
           <div className="card p-4 shadow-sm">
@@ -8,15 +21,16 @@ export function Login() {
               <p>Login to play!</p>
               <form method = "get" action = "opponents.html">
                   <div className="mb-3">
-                      <label for = "name" className = "form-label">Name</label>
-                      <input type = "text" id = "name" className="form-control" placeholder= "Your name here"/>
+                      <label htmlFor = "name" className = "form-label">Name</label>
+                      <input type = "text" id = "name" className="form-control" placeholder= "Your name here" onChange = {nameChange}/>
                   </div>
                     <div className="mb-3">
-                      <label for = "password" className = "form-label">Password</label>
-                      <input type = "text" id = "name" className="form-control" placeholder= "Your password here"/>
+                      <label htmlFor = "password" className = "form-label">Password</label>
+                      <input type = "text" id = "name" className="form-control" placeholder= "Your password here"onChange = {passChange}/>
                   </div>
                   
-                  <button type="button">Log In</button>
+                  <button type="button" onClick = {loginUser}>Log In</button>
+                  <button type="button" onClick = {loginUser}>Create</button>
               </form>
           </div>
       </main>
