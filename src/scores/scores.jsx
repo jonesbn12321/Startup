dffdc8a5863cc4242c188f9fda0f8cd451ea3f1f
Brooklyn Jones <jonesbn12321@gmail.com>
2026-02-24 13:25:16 -0700

    make scores array

--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
 
 export function Scores() {
+    React.useEffect(() => {
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+          setScores(JSON.parse(scoresText));
+        }
+      }, []);
+
+    //Create an array with React  
+    const scoreRows = [];
+    if (scores.length) {
+        for (const [i, score] of scores.entries()) {
+        scoreRows.push(
+            <tr key={i}>
+            <td>{i}</td>
+            <td>{score.name.split('@')[0]}</td>
+            <td>{score.score}</td>
+            <td>{score.date}</td>
+            </tr>
+        );
+        }
+    } else {
+        scoreRows.push(
+        <tr key='0'>
+            <td colSpan='4'>Be the first to score</td>
+        </tr>
+        );
+    }
+
+    
   return (
     <main class = "container py-4">
             <table class = "table table-striped table-hover shadow">
