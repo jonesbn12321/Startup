00a970630420c075aa37a12b57307326c4d9254f
Brooklyn Jones <jonesbn12321@gmail.com>
2026-02-24 17:39:17 -0700

    Update login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,38 +5,41 @@ import { useNavigate } from 'react-router-dom';
 export function Login({setUser}) {
     const [name, setName] = React.useState('');
     const [password, setPassword] = React.useState('');
+    const [error, setError]= React.useState('');
     const navigate = useNavigate();
 
-    function loginUser(){
-        console.log('login '+name + " "+password);
-        localStorage.setItem('user', name, password);
+    function handleSubmit(e){
+        e.preventDefault();
+        
+        if(!name||!password){
+            setError("Fill in all fields");
+            return;
+        }
+        setError('');
+        localStorage.setItem("user", name,password);
         setUser(name);
-        navigate('/opponents');
+        navigate("/opponents");
     }
 
-    function nameChange(e){
-        setName(e.target.value);
-    }
-    function passChange(e){
-        setPassword(e.target.value);
-    }
+    
+
   return (
     <main className = "container py-4">
           <div className="card p-4 shadow-sm">
               <h1>Welcome!</h1>
               <p>Login to play!</p>
-              <form onSubmit={(e) => e.preventDefault()}>
+              <form onSubmit={handleSubmit}>
                   <div className="mb-3">
                       <label htmlFor = "name" className = "form-label">Name</label>
-                      <input type = "text" id = "name" className="form-control" placeholder= "Your name here" onChange = {nameChange}/>
+                      <input type = "text" id = "name" className="form-control" placeholder= "Your name here" onChange ={(e) => setName(e.target.value)}/>
                   </div>
                     <div className="mb-3">
                       <label htmlFor = "password" className = "form-label">Password</label>
-                      <input type = "text" id = "password" className="form-control" placeholder= "Your password here"onChange = {passChange}/>
+                      <input type = "password" id = "password" className="form-control" placeholder= "Your password here"onChange = {(e) => setPassword(e.target.value)}/>
                   </div>
                   
-                  <button type="button" onClick = {loginUser}>Log In</button>
-                  <button type="button" onClick = {loginUser}>Create</button>
+                  <button type="submit" disabled={!name || !password} >Log In</button>
+                  <button type="button" >Create</button>
               </form>
           </div>
       </main>
