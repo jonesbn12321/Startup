21a656b96040f9a10349f8e6b425a9bd35ca9eb5
Brooklyn Jones <jonesbn12321@gmail.com>
2026-03-12 11:22:14 -0600

    Login functions

--- service/index.js
+++ service/index.js
@@ -17,66 +17,52 @@ const authCookieName = 'token';
 let users = [];
 let scores = [];
 
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
-//Test if it works
-app.get('/api/test', (req,res)=>{
-    res.send({message: "Backend is working"});
-})
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`);
-})
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();//Complete but no content
+});
+
+
+
 
 
 
 
 // //SIMON SERVICE EXAMPLE
-
-
-// // The scores and users are saved in memory and disappear whenever the service is restarted.
-// let users = [];
-// let scores = [];
-
-
-
-// // Router for service endpoints
-// var apiRouter = express.Router();
-// app.use(`/api`, apiRouter);
-
-// // CreateAuth a new user
-// apiRouter.post('/auth/create', async (req, res) => {
-//   if (await findUser('email', req.body.email)) {
-//     res.status(409).send({ msg: 'Existing user' });
-//   } else {
-//     const user = await createUser(req.body.email, req.body.password);
-
-//     setAuthCookie(res, user.token);
-//     res.send({ email: user.email });
-//   }
-// });
-
-// // GetAuth login an existing user
-// apiRouter.post('/auth/login', async (req, res) => {
-//   const user = await findUser('email', req.body.email);
-//   if (user) {
-//     if (await bcrypt.compare(req.body.password, user.password)) {
-//       user.token = uuid.v4();
-//       setAuthCookie(res, user.token);
-//       res.send({ email: user.email });
-//       return;
-//     }
-//   }
-//   res.status(401).send({ msg: 'Unauthorized' });
-// });
-
-// // DeleteAuth logout a user
-// apiRouter.delete('/auth/logout', async (req, res) => {
-//   const user = await findUser('token', req.cookies[authCookieName]);
-//   if (user) {
-//     delete user.token;
-//   }
-//   res.clearCookie(authCookieName);
-//   res.status(204).end();//Complete but no content
-// });
 
 // // Middleware to verify that the user is authorized to call an endpoint
 // const verifyAuth = async (req, res, next) => {
@@ -131,35 +117,39 @@ app.listen(port,()=>{
 //   return scores;
 // }
 
-// async function createUser(email, password) {
-//   const passwordHash = await bcrypt.hash(password, 10);
 
-//   const user = {
-//     email: email,
-//     password: passwordHash,
-//     token: uuid.v4(),
-//   };
-//   users.push(user);
-
-//   return user;
-// }
-
-// async function findUser(field, value) {
-//   if (!value) return null;
-
-//   return users.find((u) => u[field] === value);
-// }
-
-// // setAuthCookie in the HTTP response
-// function setAuthCookie(res, authToken) {
-//   res.cookie(authCookieName, authToken, {
-//     maxAge: 1000 * 60 * 60 * 24 * 365,
-//     secure: true,
-//     httpOnly: true,
-//     sameSite: 'strict',
-//   });
-// }
 
 // app.listen(port, () => {
 //   console.log(`Listening on port ${port}`);
 // });
+
+
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
