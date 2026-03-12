fcfc3ce75aa7d8cd5268943faafa05f01b159a1f
Brooklyn Jones <jonesbn12321@gmail.com>
2026-03-12 10:58:07 -0600

    Add middleware and tests

--- service/index.js
+++ service/index.js
@@ -4,3 +4,156 @@ const bcrypt = require('bcrypt.js');
 const uuid = require('uuid');
 
 const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+//Middleware
+app.use(express.json);
+app.use(cookieParser());
+app.use(express.static('public'));
+
+//Test if it works
+app.get('api/test', (req,res)=>{
+    res.send({message: "Backend is working"});
+})
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+})
+
+
+
+
+// //SIMON SERVICE EXAMPLE
+
+
+// // The scores and users are saved in memory and disappear whenever the service is restarted.
+// let users = [];
+// let scores = [];
+
+
+
+// // Router for service endpoints
+// var apiRouter = express.Router();
+// app.use(`/api`, apiRouter);
+
+// // CreateAuth a new user
+// apiRouter.post('/auth/create', async (req, res) => {
+//   if (await findUser('email', req.body.email)) {
+//     res.status(409).send({ msg: 'Existing user' });
+//   } else {
+//     const user = await createUser(req.body.email, req.body.password);
+
+//     setAuthCookie(res, user.token);
+//     res.send({ email: user.email });
+//   }
+// });
+
+// // GetAuth login an existing user
+// apiRouter.post('/auth/login', async (req, res) => {
+//   const user = await findUser('email', req.body.email);
+//   if (user) {
+//     if (await bcrypt.compare(req.body.password, user.password)) {
+//       user.token = uuid.v4();
+//       setAuthCookie(res, user.token);
+//       res.send({ email: user.email });
+//       return;
+//     }
+//   }
+//   res.status(401).send({ msg: 'Unauthorized' });
+// });
+
+// // DeleteAuth logout a user
+// apiRouter.delete('/auth/logout', async (req, res) => {
+//   const user = await findUser('token', req.cookies[authCookieName]);
+//   if (user) {
+//     delete user.token;
+//   }
+//   res.clearCookie(authCookieName);
+//   res.status(204).end();//Complete but no content
+// });
+
+// // Middleware to verify that the user is authorized to call an endpoint
+// const verifyAuth = async (req, res, next) => {
+//   const user = await findUser('token', req.cookies[authCookieName]);
+//   if (user) {
+//     next();
+//   } else {
+//     res.status(401).send({ msg: 'Unauthorized' });
+//   }
+// };
+
+// // GetScores
+// apiRouter.get('/scores', verifyAuth, (_req, res) => {
+//   res.send(scores);
+// });
+
+// // SubmitScore
+// apiRouter.post('/score', verifyAuth, (req, res) => {
+//   scores = updateScores(req.body);
+//   res.send(scores);
+// });
+
+// // Default error handler
+// app.use(function (err, req, res, next) {
+//   res.status(500).send({ type: err.name, message: err.message });
+// });
+
+// // Return the application's default page if the path is unknown
+// app.use((_req, res) => {
+//   res.sendFile('index.html', { root: 'public' });
+// });
+
+// // updateScores considers a new score for inclusion in the high scores.
+// function updateScores(newScore) {
+//   let found = false;
+//   for (const [i, prevScore] of scores.entries()) {
+//     if (newScore.score > prevScore.score) {
+//       scores.splice(i, 0, newScore);
+//       found = true;
+//       break;
+//     }
+//   }
+
+//   if (!found) {
+//     scores.push(newScore);
+//   }
+
+//   if (scores.length > 10) {
+//     scores.length = 10;
+//   }
+
+//   return scores;
+// }
+
+// async function createUser(email, password) {
+//   const passwordHash = await bcrypt.hash(password, 10);
+
+//   const user = {
+//     email: email,
+//     password: passwordHash,
+//     token: uuid.v4(),
+//   };
+//   users.push(user);
+
+//   return user;
+// }
+
+// async function findUser(field, value) {
+//   if (!value) return null;
+
+//   return users.find((u) => u[field] === value);
+// }
+
+// // setAuthCookie in the HTTP response
+// function setAuthCookie(res, authToken) {
+//   res.cookie(authCookieName, authToken, {
+//     maxAge: 1000 * 60 * 60 * 24 * 365,
+//     secure: true,
+//     httpOnly: true,
+//     sameSite: 'strict',
+//   });
+// }
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
