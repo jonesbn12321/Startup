391562ccb0342edeeac00d91219816146439de05
Brooklyn Jones <jonesbn12321@gmail.com>
2026-03-24 21:31:47 -0600

    Get rid of period on db.js

--- service/db.js
+++ service/db.js
@@ -1,4 +1,4 @@
-const{ MongoClient }= require('mongo.db');
+const{ MongoClient }= require('mongodb');
 const config = require('./dbConfig.json');
 
 const url =  `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
