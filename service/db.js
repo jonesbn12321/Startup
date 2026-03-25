e650399796eb7de786126eaccd5c20735ce0e077
Brooklyn Jones <jonesbn12321@gmail.com>
2026-03-24 21:17:22 -0600

    more db

--- service/db.js
+++ service/db.js
@@ -5,3 +5,22 @@ const url =  `mongodb+srv://${config.userName}:${config.password}@${config.hostn
 const client = new MongoClient(url);
 
 const db = client.db('startup');
+const userCollection = db.collection('user');
+const scoreCollection = db.collection('score');
+
+// Test the connection and exit the process if it fails
+async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    client.close();
+    process.exit(1);
+  }
+}
+
+testConnection();
+module.exports = {userCollection, scoreCollection};
+
+
