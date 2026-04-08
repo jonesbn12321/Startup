abfafbef9a4a454eb13c0423c34cd3a8ff8be896
Brooklyn Jones <jonesbn12321@gmail.com>
2026-04-07 21:38:29 -0600

    debug some more

--- index.jsx
+++ index.jsx
@@ -4,6 +4,5 @@ import App from './src/app';
 import {WSProvider} from './src/webSocket'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-<WSProvider>
-    root.render(<App />);
-</WSProvider>
+root.render(<WSProvider><App /></WSProvider>);
+
--- src/opponents/opponents.jsx
+++ src/opponents/opponents.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import './opponents.css';
-import{useWS} from '..webSocket';
+import{useWS} from '../webSocket';
 
 
 
